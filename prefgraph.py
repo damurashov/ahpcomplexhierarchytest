0c950cc1fb9884c0106df3d47ec0e070e45fcee9
DmitryMurashov <dmtr.murashov@gmail.com>
2021-10-23 19:24:19 +0300

    [prefgraph] Impl | Verification

--- prefgraph.py
+++ prefgraph.py
@@ -27,6 +27,7 @@ satisfies the requirements. Therefore, in cases when it's not the BEHAVIOR IS UN
 from functools import reduce
 from ahpy.ahpy import ahpy
 import copy
+import itertools
 
 
 class Graph:
@@ -115,12 +116,16 @@ class Graph:
 		Format: {context1: {alt1: weight, alt2: weight}, context2: {...}}
 		:return: dict of assessed vertices (extended).
 		"""
-		print('--->> ' + context)
+
+		def __trace(s):
+			pass
+
+		__trace('--->> ' + context)
 		children = self.get_children_of(context)
-		print("children: " + str(children))
+		__trace("children: " + str(children))
 
 		if not children:
-			print('<<---')
+			__trace('<<--- ' + context)
 			return assessed
 
 		for child in children:
@@ -130,14 +135,14 @@ class Graph:
 		compare = ahpy.Compare(context, self.graph[context])
 		lower_contexts = [ahpy.Compare(child, Graph.to_pairwise(assessed[child])) for child in children if child in assessed.keys()]
 
-		print("lower contexts: " + str(lower_contexts))
+		__trace("lower contexts: " + str(lower_contexts))
 
 		if lower_contexts:
 			compare.add_children(lower_contexts)
 
 		assessed[context] = compare.target_weights
 
-		print('<<---')
+		__trace('<<---' + context)
 		return assessed
 
 	def get_weights(self):
@@ -147,19 +152,155 @@ class Graph:
 		return self._get_weights(self.root)[self.root]
 
 
+def ahpy_simple():
+	experience_comparisons = {('Moll', 'Nell'): 1 / 4, ('Moll', 'Sue'): 4, ('Nell', 'Sue'): 9}
+	education_comparisons = {('Moll', 'Nell'): 3, ('Moll', 'Sue'): 1 / 5, ('Nell', 'Sue'): 1 / 7}
+	charisma_comparisons = {('Moll', 'Nell'): 5, ('Moll', 'Sue'): 9, ('Nell', 'Sue'): 4}
+	age_comparisons = {('Moll', 'Nell'): 1 / 3, ('Moll', 'Sue'): 5, ('Nell', 'Sue'): 9}
+	criteria_comparisons = {('Experience', 'Education'): 4, ('Experience', 'Charisma'): 3, ('Experience', 'Age'): 7,
+							('Education', 'Charisma'): 1 / 3, ('Education', 'Age'): 3,
+							('Charisma', 'Age'): 5}
+
+	experience = ahpy.Compare('Experience', experience_comparisons, precision=3, random_index='saaty')
+	education = ahpy.Compare('Education', education_comparisons, precision=3, random_index='saaty')
+	charisma = ahpy.Compare('Charisma', charisma_comparisons, precision=3, random_index='saaty')
+	age = ahpy.Compare('Age', age_comparisons, precision=3, random_index='saaty')
+	criteria = ahpy.Compare('Criteria', criteria_comparisons, precision=3, random_index='saaty')
+
+	criteria.add_children([experience, education, charisma, age])
+
+	print(criteria.target_weights)
+
+
+def graph_simple():
+	graph = Graph("Criteria")
+	graph.set_weights("Experience", {('Moll', 'Nell'): 1 / 4, ('Moll', 'Sue'): 4, ('Nell', 'Sue'): 9})
+	graph.set_weights("Education", {('Moll', 'Nell'): 3, ('Moll', 'Sue'): 1 / 5, ('Nell', 'Sue'): 1 / 7})
+	graph.set_weights("Charisma", {('Moll', 'Nell'): 5, ('Moll', 'Sue'): 9, ('Nell', 'Sue'): 4})
+	graph.set_weights("Age", {('Moll', 'Nell'): 1 / 3, ('Moll', 'Sue'): 5, ('Nell', 'Sue'): 9})
+	graph.set_weights("Criteria", {('Experience', 'Education'): 4, ('Experience', 'Charisma'): 3,
+								   ('Experience', 'Age'): 7, ('Education', 'Charisma'): 1 / 3, ('Education', 'Age'): 3,
+								   ('Charisma', 'Age'): 5})
+
+	print(graph.get_weights())
+
+
+def ahpy_complex():
+	def m(elements, judgments):
+		return dict(zip(elements, judgments))
+
+	cri = ('Cost', 'Safety', 'Style', 'Capacity')
+	c_cri = list(itertools.combinations(cri, 2))
+	criteria = ahpy.Compare('Criteria', m(c_cri, (3, 7, 3, 9, 1, 1 / 7)), 3)
+
+	alt = ('Accord Sedan', 'Accord Hybrid', 'Pilot', 'CR-V', 'Element', 'Odyssey')
+	pairs = list(itertools.combinations(alt, 2))
+
+	costs = ('Price', 'Fuel', 'Maintenance', 'Resale')
+	c_pairs = list(itertools.combinations(costs, 2))
+	cost = ahpy.Compare('Cost', m(c_pairs, (2, 5, 3, 2, 2, .5)), precision=3)
+
+	cost_price_m = (9, 9, 1, 0.5, 5, 1, 1 / 9, 1 / 9, 1 / 7, 1 / 9, 1 / 9, 1 / 7, 1 / 2, 5, 6)
+	cost_price = ahpy.Compare('Price', m(pairs, cost_price_m), 3)
+
+	# cost_fuel_m = (1/1.13, 1.41, 1.15, 1.24, 1.19, 1.59, 1.3, 1.4, 1.35, 1/1.23, 1/1.14, 1/1.18, 1.08, 1.04, 1/1.04)
+	cost_fuel_m = (31, 35, 22, 27, 25, 26)
+	# cost_fuel = ahpy.Compare('Fuel', m(pairs, cost_fuel_m), 3)
+	cost_fuel = ahpy.Compare('Fuel', m(alt, cost_fuel_m), 3)
+
+	# cost_resale_m = (3, 4, 1 / 2, 2, 2, 2, 1 / 5, 1, 1, 1 / 6, 1 / 2, 1 / 2, 4, 4, 1)
+	cost_resale_m = (0.52, 0.46, 0.44, 0.55, 0.48, 0.48)
+	# cost_resale = ahpy.Compare('Resale', m(pairs, cost_resale_m), 3)
+	cost_resale = ahpy.Compare('Resale', m(alt, cost_resale_m), 3)
+
+	cost_maint_m = (1.5, 4, 4, 4, 5, 4, 4, 4, 5, 1, 1.2, 1, 1, 3, 2)
+	cost_maint = ahpy.Compare('Maintenance', m(pairs, cost_maint_m), 3)
+
+	safety_m = (1, 5, 7, 9, 1 / 3, 5, 7, 9, 1 / 3, 2, 9, 1 / 8, 2, 1 / 8, 1 / 9)
+	safety = ahpy.Compare('Safety', m(pairs, safety_m), 3)
+
+	style_m = (1, 7, 5, 9, 6, 7, 5, 9, 6, 1 / 6, 3, 1 / 3, 7, 5, 1 / 5)
+	style = ahpy.Compare('Style', m(pairs, style_m), 3)
+
+	capacity = ahpy.Compare('Capacity', {('Cargo', 'Passenger'): 0.2})
+
+	# capacity_pass_m = (1, 1 / 2, 1, 3, 1 / 2, 1 / 2, 1, 3, 1 / 2, 2, 6, 1, 3, 1 / 2, 1 / 6)
+	capacity_pass_m = (5, 5, 8, 5, 4, 8)
+	# capacity_pass = ahpy.Compare('Passenger', m(pairs, capacity_pass_m), 3)
+	capacity_pass = ahpy.Compare('Passenger', m(alt, capacity_pass_m), 3)
+
+	# capacity_cargo_m = (1, 1 / 2, 1 / 2, 1 / 2, 1 / 3, 1 / 2, 1 / 2, 1 / 2, 1 / 3, 1, 1, 1 / 2, 1, 1 / 2, 1 / 2)
+	capacity_cargo_m = (14, 14, 87.6, 72.9, 74.6, 147.4)
+	# capacity_cargo = ahpy.Compare('Cargo', m(pairs, capacity_cargo_m), precision=3)
+	capacity_cargo = ahpy.Compare('Cargo', m(alt, capacity_cargo_m), precision=3)
+
+	cost.add_children([cost_price, cost_fuel, cost_maint, cost_resale])
+	capacity.add_children([capacity_cargo, capacity_pass])
+	criteria.add_children([cost, safety, style, capacity])
+
+	print(criteria.target_weights)
+
+
+def graph_complex():
+	def m(elements, judgments):
+		return dict(zip(elements, judgments))
+
+	cri = ('Cost', 'Safety', 'Style', 'Capacity')
+	c_cri = list(itertools.combinations(cri, 2))
+	criteria = m(c_cri, (3, 7, 3, 9, 1, 1 / 7))
+
+	alt = ('Accord Sedan', 'Accord Hybrid', 'Pilot', 'CR-V', 'Element', 'Odyssey')
+	pairs = list(itertools.combinations(alt, 2))
+
+	costs = ('Price', 'Fuel', 'Maintenance', 'Resale')
+	c_pairs = list(itertools.combinations(costs, 2))
+	cost = m(c_pairs, (2, 5, 3, 2, 2, .5))
+
+	cost_price_m = (9, 9, 1, 0.5, 5, 1, 1 / 9, 1 / 9, 1 / 7, 1 / 9, 1 / 9, 1 / 7, 1 / 2, 5, 6)
+	cost_price = m(pairs, cost_price_m)
+
+	cost_fuel_m = (31, 35, 22, 27, 25, 26)
+	cost_fuel = Graph.to_pairwise(m(alt, cost_fuel_m))
+
+	cost_resale_m = (0.52, 0.46, 0.44, 0.55, 0.48, 0.48)
+	cost_resale = m(alt, cost_resale_m)
+	cost_resale = Graph.to_pairwise(cost_resale)
+
+	cost_maint_m = (1.5, 4, 4, 4, 5, 4, 4, 4, 5, 1, 1.2, 1, 1, 3, 2)
+	cost_maint = m(pairs, cost_maint_m)
+
+	safety_m = (1, 5, 7, 9, 1 / 3, 5, 7, 9, 1 / 3, 2, 9, 1 / 8, 2, 1 / 8, 1 / 9)
+	safety = m(pairs, safety_m)
+
+	style_m = (1, 7, 5, 9, 6, 7, 5, 9, 6, 1 / 6, 3, 1 / 3, 7, 5, 1 / 5)
+	style = m(pairs, style_m)
+
+	capacity = {('Cargo', 'Passenger'): 0.2}
+
+	capacity_pass_m = (5, 5, 8, 5, 4, 8)
+	capacity_pass = Graph.to_pairwise(m(alt, capacity_pass_m))
+
+	capacity_cargo_m = (14, 14, 87.6, 72.9, 74.6, 147.4)
+	capacity_cargo = Graph.to_pairwise(m(alt, capacity_cargo_m))
+
+	graph = Graph("Criteria")
+	graph.set_weights("Criteria", criteria)
+	graph.set_weights("Cost", cost)
+	graph.set_weights("Price", cost_price)
+	graph.set_weights("Fuel", cost_fuel)
+	graph.set_weights("Resale", cost_resale)
+	graph.set_weights("Maintenance", cost_maint)
+	graph.set_weights("Safety", safety)
+	graph.set_weights("Style", style)
+	graph.set_weights("Capacity", capacity)
+	graph.set_weights("Passenger", capacity_pass)
+	graph.set_weights("Cargo", capacity_cargo)
+
+	print(graph.get_weights())
+
+
 if __name__ == "__main__":
-	graph = Graph("target")
-	graph.set_weights("target", Graph.to_pairwise({"b1": 1, "b2": 3, "b3": 4}))
-	graph.set_weights("b1", Graph.to_pairwise({"c1": 30, "c2": 50}))
-	graph.set_weights("b2", Graph.to_pairwise({"c2": 30, "c3": 50}))
-	graph.set_weights("b3", Graph.to_pairwise({"c1": 10, "c3": 100}))
-
-	graph_copy = copy.deepcopy(graph)
-	graph_copy.set_weights("c1", Graph.to_pairwise({"Alternative 1": 10, "Alternative 2": 20}))
-	graph_copy.set_weights("c2", Graph.to_pairwise({"Alternative 1": 10, "Alternative 2": 1}))
-	graph_copy.set_weights("c3", Graph.to_pairwise({"Alternative 1": 10, "Alternative 2": 3}))
-
-	print(graph.graph)
-	print(graph_copy.graph)
-
-	print(graph_copy.get_weights())
+	# ahpy_simple()
+	# graph_simple()
+	ahpy_complex()
+	graph_complex()
